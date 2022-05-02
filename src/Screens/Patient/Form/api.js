193c327f9c7af36fd545d777a4def4173e6fce81
Ankitjainwebnexus <ankitjain.webnexus@gmail.com>
2022-05-02 10:51:12 +0530

    Worked on sick leave form

---
+++ src/Screens/Patient/Form/api.js
@@ -0,0 +1,18 @@
+// Submit form details with validations
+// const handleEvalSubmit = () => {
+//   setErrorChrMsg('');
+//   var data = updateQues;
+//   if (
+//     data.headache_painbegin_back === true ||
+//     data.headache_painbegin_front === true ||
+//     data.headache_painbegin_left === true ||
+//     data.headache_painbegin_right === true ||
+//     data.headache_painbegin_top === true
+//   ) {
+//     console.log('updateQues', updateQues);
+//     setUpdateQues({});
+//   } else {
+//     setError_section(1);
+//     setErrorChrMsg('Please select pain begin');
+//   }
+// };
