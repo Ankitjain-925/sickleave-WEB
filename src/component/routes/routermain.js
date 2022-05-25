0ff1aa91339f824e492e7cc01e2d13ee5dec79a2
Ankitjainwebnexus <ankitjain.webnexus@gmail.com>
2022-05-25 13:42:43 +0530

    Worked on video call section

--- src/component/routes/routermain.js
+++ src/component/routes/routermain.js
@@ -56,7 +56,7 @@ class Routermain extends Component {
 
             <Route
               exact
-              path="/video-call/:id"
+              path="/video-call/:user_id/:session_id"
               render={(props) => <VideoCall {...props} />}
             />
             <Route
